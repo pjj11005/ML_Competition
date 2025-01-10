39d2157a6f9ddf2099561fc8fe2c243c4567f220
Jeongjae <107382641+pjj11005@users.noreply.github.com>
2025-01-10 19:55:40 +0900

    feat: Update getModules.py

--- Dacon/IC50_activity_prediction/structure/utils/getModules.py
+++ Dacon/IC50_activity_prediction/structure/utils/getModules.py
@@ -1,8 +1,17 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, SubsetRandomSampler
 from rdkit import Chem
 from rdkit.Chem import Descriptors
+
+from dataset.moleculedataset import MoleculeDataset
+
+
+# pIC50 -> IC50
+def pIC50_to_IC50(pic50_values):
+    """Convert pIC50 values to IC50 (nM)."""
+    return 10 ** (9 - pic50_values)
 
 
 # 분자 특성
@@ -50,30 +59,60 @@ def extract_features(smiles):
         return None
 
 
-# 데이터 로드
-train_data = pd.read_csv("train.csv")
-test_data = pd.read_csv("test.csv")
+# 전처리된 train, test 반환
+def load_train_test():
+    # 데이터 로드
+    train_data = pd.read_csv('../data/train.csv')
+    test_data = pd.read_csv('../data/test.csv')
 
-# feature + target, X_test
-X_train = train_data["Smiles"]
-y_train = train_data["pIC50"]
-X_test = test_data["Smiles"]
+    # feature + target, X_test
+    X_train = train_data["Smiles"]
+    y_train = train_data["pIC50"]
+    X_test = test_data["Smiles"]
 
-# 특성 추출
-X_train_features = X_train.apply(extract_features)
-X_test_features = X_test.apply(extract_features)
+    # 특성 추출
+    X_train_features = X_train.apply(extract_features)
+    X_test_features = X_test.apply(extract_features)
 
-# 결측치 제거
-X_train_features = X_train_features[X_train_features.notna()]
-y_train = y_train[X_train_features.index]
-X_test_features = X_test_features[X_test_features.notna()]
+    # 결측치 제거
+    X_train_features = X_train_features[X_train_features.notna()]
+    y_train = y_train[X_train_features.index]
+    X_test_features = X_test_features[X_test_features.notna()]
 
-# 데이터 프레임으로 변환
-X_train_features = pd.DataFrame(X_train_features.to_list())
-X_test_features = pd.DataFrame(X_test_features.to_list())
+    # 데이터 프레임으로 변환
+    X_train_features = pd.DataFrame(X_train_features.to_list())
+    X_test_features = pd.DataFrame(X_test_features.to_list())
 
-# 수치형 스케일링
-scaler = StandardScaler()
-numerical_columns = X_train_features.columns[X_train_features.dtypes != "object"]
-X_train_features[numerical_columns] = scaler.fit_transform(X_train_features[numerical_columns])
-X_test_features[numerical_columns] = scaler.transform(X_test_features[numerical_columns])
+    # 수치형 스케일링
+    scaler = StandardScaler()
+    numerical_columns = X_train_features.columns[X_train_features.dtypes != "object"]
+    X_train_features[numerical_columns] = scaler.fit_transform(X_train_features[numerical_columns])
+    X_test_features[numerical_columns] = scaler.transform(X_test_features[numerical_columns])
+
+    return X_train, X_test, X_train_features, X_test_features, y_train
+
+
+# dataset 
+def load_datasets(X_train, X_test, X_train_features, X_test_features, y_train,  tokenizer):
+    train_dataset = MoleculeDataset(X_train, X_train_features, y_train, tokenizer)
+    test_dataset = MoleculeDataset(X_test, X_test_features, tokenizer=tokenizer)
+
+    return train_dataset, test_dataset
+
+
+# dataloader
+def load_dataloaders(tokenizer, batch_size):
+    X_train, X_test, X_train_features, X_test_features, y_train = load_train_test()
+    train_dataset, test_dataset = load_datasets(X_train, X_test, X_train_features, X_test_features, y_train,  tokenizer)
+
+    # train data -> train and validation sets
+    train_idx, val_idx = train_test_split(range(len(train_dataset)), test_size=0.2, random_state=42)
+    train_subsampler = SubsetRandomSampler(train_idx)
+    val_subsampler = SubsetRandomSampler(val_idx)
+
+    # data loaders 생성 (train, valid, test)
+    train_loader = DataLoader(train_dataset, batch_size, sampler=train_subsampler)
+    val_loader = DataLoader(train_dataset, batch_size, sampler=val_subsampler)
+    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
